43bc50fd316d65def7442cfc5b0179fdff79e005
tamara-laranga <tamaralaranga@gmail.com>
2026-01-19 17:26:19 +0100

    Fix nvim-treesitter config

--- lua/tamara/lazy/treesitter.lua
+++ lua/tamara/lazy/treesitter.lua
@@ -1,34 +1,41 @@
-require("nvim-treesitter.configs").setup({
-	-- List of parsers to install
-	ensure_installed = {
-		"c",
-		"cpp",
-		"lua",
-		"vim",
-		"vimdoc",
-		"query",
-		"python",
-		"markdown",
-		"markdown_inline",
-	},
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            -- List of parsers to install
+            ensure_installed = {
+                "c",
+                "cpp",
+                "lua",
+                "vim",
+                "vimdoc",
+                "query",
+                "python",
+                "markdown",
+                "markdown_inline",
+            },
 
-	sync_install = false,
-	auto_install = true,
+            sync_install = false,
+            auto_install = true,
 
-	highlight = {
-		enable = true,
+            highlight = {
+                enable = true,
 
-		additional_vim_regex_highlighting = false,
-	},
+                additional_vim_regex_highlighting = false,
+            },
 
-	indent = {
-		enable = true,
-	},
+            indent = {
+                enable = true,
+            },
 
-	-- Config for pair parenthesis and context highlight
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
-})
+            -- Config for pair parenthesis and context highlight
+            context_commentstring = {
+                enable = true,
+                enable_autocmd = false,
+            },
+        })
+    end,
+}
 
